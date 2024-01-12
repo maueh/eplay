fc07bb62edd8d8f60f5b34ee5fd34f9b7b2d7d58
Marcel <uehdev@gmail.com>
2024-01-12 07:30:00 -0300

    Commit da parte 2 finalizada

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -30,7 +30,7 @@ export const Imagem = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(0, 0, 0, 0.5);
     content: '';
   }
 `
